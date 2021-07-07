9df3e96f8aab864b098af34978644b38d512ff32
Nathan Formentin <nathan.formentin@mercadolivre.com>
2021-07-07 15:47:48 -0300

    solucionando um warning relacionado ao Lombok (@EqualsAndHashCode(callSuper = true)).

--- src/main/java/com/mercadolivre/desafio_spring/dto/FollowersCountedDTO.java
+++ src/main/java/com/mercadolivre/desafio_spring/dto/FollowersCountedDTO.java
@@ -4,11 +4,13 @@ package com.mercadolivre.desafio_spring.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
 public class FollowersCountedDTO  extends  UserDTO{
     @JsonProperty("followers_count")
     private int followersCount;
