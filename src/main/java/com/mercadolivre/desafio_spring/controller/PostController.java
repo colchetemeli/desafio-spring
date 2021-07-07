014cbfe92ac2d6505b5762d787a34bb28bdf6346
JhonasML <jhonas.mutton@mercadolivre.com>
2021-07-07 16:19:12 -0300

    adicionado post controller

---
+++ src/main/java/com/mercadolivre/desafio_spring/controller/PostController.java
@@ -0,0 +1,53 @@
+package com.mercadolivre.desafio_spring.controller;
+
+import com.mercadolivre.desafio_spring.dto.*;
+import com.mercadolivre.desafio_spring.service.IPostsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/products")
+public class PostController implements IPostController{
+
+    private IPostsService postsService;
+
+    @Autowired
+    public PostController(IPostsService postsService) {
+        this.postsService = postsService;
+    }
+
+    //US 0005
+    @Override
+    @PostMapping("/newpost")
+    public void createPost(@RequestBody PostDTO post) {
+        postsService.createPost(post);
+    }
+
+    //US 0006
+    @Override
+    @GetMapping("/followed/{userId}/list")
+    public FollowedPostsDTO getFollowedPosts(@PathVariable int userId, @RequestParam(required = false) String order) {
+        return postsService.getFollowedPosts(userId, order);
+    }
+
+    //US 0010
+    @Override
+    @PostMapping("/newpromopost")
+    public void createPromoPost(@RequestBody PromoPostDTO post) {
+        postsService.createPromoPost(post);
+    }
+
+    //US 0011
+    @Override
+    @GetMapping("/{userId}/countPromo")
+    public UserPromosCountedDTO countPromoByUser(@PathVariable int userId) {
+        return postsService.countPromoByUser(userId);
+    }
+
+    //US 0012
+    @Override
+    @GetMapping("/{userId}/list")
+    public UserPromosDTO getPromosByUser(@PathVariable int userId) {
+        return postsService.getPromosByUser(userId);
+    }
+}
