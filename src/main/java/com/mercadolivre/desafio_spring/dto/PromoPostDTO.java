b3d4f1fb40be606f1c958cd58c606ab1feed0495
vicrodrigues96 <victor.vrodrigues@mercadolivre.com>
2021-07-07 16:21:47 -0300

    ajustando dto e entidade do post

--- src/main/java/com/mercadolivre/desafio_spring/dto/PromoPostDTO.java
+++ src/main/java/com/mercadolivre/desafio_spring/dto/PromoPostDTO.java
@@ -1,6 +1,28 @@
 package com.mercadolivre.desafio_spring.dto;
 
+import com.mercadolivre.desafio_spring.entity.Post;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Accessors(chain = true)
 public class PromoPostDTO extends PostDTO{
     private boolean hasPromo;
     private double discount;
+
+    public Post toEntity() {
+        return new Post()
+                .setUserId(super.getUserId())
+                .setId(super.getId())
+                .setDate(super.getDate())
+                .setDetail(super.getDetail())
+                .setCategory(super.getCategory())
+                .setPrice(super.getPrice())
+                .setHasPromo(this.hasPromo)
+                .setDiscount(this.discount);
+    }
 }
--- src/main/java/com/mercadolivre/desafio_spring/entity/Post.java
+++ src/main/java/com/mercadolivre/desafio_spring/entity/Post.java
@@ -3,6 +3,7 @@ package com.mercadolivre.desafio_spring.entity;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.mercadolivre.desafio_spring.dto.PostDTO;
+import com.mercadolivre.desafio_spring.dto.PromoPostDTO;
 import com.mercadolivre.desafio_spring.util.Constants;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -36,5 +37,19 @@ public class Post {
                 .setDetail(this.detail)
                 .setCategory(this.category)
                 .setPrice(this.price);
+    }
+
+    public PromoPostDTO ToPromoPostDTO() {
+        PromoPostDTO promoPostDTO = new PromoPostDTO();
+        promoPostDTO.setUserId(this.userId);
+        promoPostDTO.setId(this.id);
+        promoPostDTO.setDate(this.date);
+        promoPostDTO.setDetail(this.detail);
+        promoPostDTO.setCategory(this.category);
+        promoPostDTO.setPrice(this.price);
+        promoPostDTO.setHasPromo(this.hasPromo);
+        promoPostDTO.setDiscount(this.discount);
+
+        return promoPostDTO;
     }
 }
