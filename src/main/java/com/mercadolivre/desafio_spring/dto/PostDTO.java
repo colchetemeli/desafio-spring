f3c4e5ed367879dd0b6e9d5b3f921fd45ff2fb95
vicrodrigues96 <victor.vrodrigues@mercadolivre.com>
2021-07-07 12:12:44 -0300

    atualizando entidade de dto de posts

--- src/main/java/com/mercadolivre/desafio_spring/dto/PostDTO.java
+++ src/main/java/com/mercadolivre/desafio_spring/dto/PostDTO.java
@@ -1,10 +1,12 @@
 package com.mercadolivre.desafio_spring.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.mercadolivre.desafio_spring.entity.Post;
 import com.mercadolivre.desafio_spring.entity.Product;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 
 import java.util.Date;
 
@@ -13,11 +15,24 @@ import java.util.Date;
 @AllArgsConstructor
 @NoArgsConstructor
 public class PostDTO {
+
+    private int userId;
     @JsonProperty("id_post")
     private int id;
-    private int userId;
     private Date date;
     private Product detail;
     private int category;
     private double price;
+
+    public Post toEntity() {
+        return new Post()
+                .setUserId(this.userId)
+                .setId(this.id)
+                .setDate(this.date)
+                .setDetail(this.detail)
+                .setCategory(this.category)
+                .setPrice(this.price)
+                .setHasPromo(false)
+                .setDiscount(0);
+    }
 }
--- src/main/java/com/mercadolivre/desafio_spring/entity/Post.java
+++ src/main/java/com/mercadolivre/desafio_spring/entity/Post.java
@@ -1,16 +1,21 @@
 package com.mercadolivre.desafio_spring.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 
 import java.util.Date;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Accessors(chain = true)
 public class Post {
 
+    private int userId;
+    @JsonProperty("id_post")
     private int id;
     private Date date;
     private Product detail;
