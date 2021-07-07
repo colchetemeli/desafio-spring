114d8e02df4e1664578ff638b13a16ce666342eb
Nathan Formentin <nathan.formentin@mercadolivre.com>
2021-07-07 15:47:48 -0300

    solucionando um warning relacionado ao Lombok (@EqualsAndHashCode(callSuper = true)).

--- src/main/java/com/mercadolivre/desafio_spring/service/UserService.java
+++ src/main/java/com/mercadolivre/desafio_spring/service/UserService.java
@@ -6,29 +6,36 @@ import com.mercadolivre.desafio_spring.dto.UserFollowedDTO;
 import com.mercadolivre.desafio_spring.dto.UserFollowersDTO;
 import com.mercadolivre.desafio_spring.entity.User;
 import com.mercadolivre.desafio_spring.repository.IUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class UserService implements IUserService {
 
     final IUserRepository userRepository;
+    private static final String REVERSE_ORDERING = "name_desc";
 
+    @Autowired
     public UserService(IUserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
     @Override
-    public void follow(int userId, int userIdToFollow) {
+    public void follow(int  userId, int userIdToFollow) {
         if (userId != userIdToFollow) {
             User userFollowing = userRepository.fetchById(userId);
             User userToFollow = userRepository.fetchById(userIdToFollow);
 
             userFollowing.getFollowed().add(userIdToFollow);
             userToFollow.getFollowers().add(userId);
+
+            userRepository.update(userId, userFollowing);
+            userRepository.update(userIdToFollow, userToFollow);
         }
     }
 
@@ -36,47 +43,57 @@ public class UserService implements IUserService {
     public void unfollow(int userId, int userIdToUnfollow) {
         User userFollowing = userRepository.fetchById(userId);
         User userToUnfollow = userRepository.fetchById(userIdToUnfollow);
+
         userFollowing.getFollowed().remove(Integer.valueOf(userIdToUnfollow));
         userToUnfollow.getFollowers().remove(Integer.valueOf(userId));
+
+        userRepository.update(userId, userFollowing);
+        userRepository.update(userIdToUnfollow, userToUnfollow);
+
     }
 
     @Override
     public FollowersCountedDTO countFollowers(int userId) {
         User user = userRepository.fetchById(userId);
         int numberOfFollowers = user.getFollowers().size();
+
         return new FollowersCountedDTO(userId, user.getName(), numberOfFollowers);
     }
 
     @Override
     public UserFollowersDTO getFollowers(int userId, String order) {
         User user = userRepository.fetchById(userId);
-        List<UserDTO> followers = getAllSorted(user.getFollowers(), order);
+
+        List<UserDTO> followers = getAllUsers(user.getFollowers());
+        sortFollowers(followers, order);
+
         return new UserFollowersDTO(user.getId(), user.getName(), followers);
     }
 
     @Override
     public UserFollowedDTO getFollowed(int userId, String order) {
         User user = userRepository.fetchById(userId);
-        List<UserDTO> followed = getAllSorted(user.getFollowed(), order);
+        List<UserDTO> followed = getAllUsers(user.getFollowed());
+        sortFollowers(followed, order);
+
         return new UserFollowedDTO(user.getId(), user.getName(), followed);
     }
 
     private void sortFollowers(List<UserDTO> followersDTO, String order) {
-        if (order.equals("name_desc")) {
+        if (Objects.equals(order, REVERSE_ORDERING)) {
             followersDTO.sort(Comparator.comparing(UserDTO::getUserName).reversed());
         } else {
             followersDTO.sort(Comparator.comparing(UserDTO::getUserName));
         }
     }
 
-    private List<UserDTO> getAllSorted(List<Integer> followersIds, String order) {
+    private List<UserDTO> getAllUsers(List<Integer> followersIds) {
         List<UserDTO> followersDTO = new ArrayList<>();
         for (Integer followerId : followersIds) {
             User follower = userRepository.fetchById(followerId);
             followersDTO.add(new UserDTO(follower.getId(), follower.getName()));
         }
 
-        sortFollowers(followersDTO, order);
         return followersDTO;
     }
 }
