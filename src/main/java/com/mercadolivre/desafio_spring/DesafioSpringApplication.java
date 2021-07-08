e44d3b1c0f13ca3d938bf1d63b786ef3c2902e20
Nathan Formentin <nathan.formentin@mercadolivre.com>
2021-07-08 14:45:09 -0300

    adicionando validacao se o usuario segue a si mesmo e proibindo mais de um follow no mesmo usuario.

--- src/main/java/com/mercadolivre/desafio_spring/DesafioSpringApplication.java
+++ src/main/java/com/mercadolivre/desafio_spring/DesafioSpringApplication.java
@@ -1,13 +1,2 @@
-package com.mercadolivre.desafio_spring;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class DesafioSpringApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(DesafioSpringApplication.class, args);
-    }
-
+package com.mercadolivre.desafio_spring;public class DesafioSpringApplication {
 }
